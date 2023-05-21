cc7b3fb6caacc7d873b94d2a4e66b610ea061e7f
keven panchal <54621559+xtreme124@users.noreply.github.com>
2023-05-21 16:13:08 +0530

    Add files via upload

---
+++ src/component/navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+
+        <div className='container'>
+
+            <div className="leftnavbar">
+                <div className="title">BTS Gallery</div>
+            </div>
+
+            <div className="middlenavbar">
+                <span className="jk">
+                    <img src="jungkook.jpg" alt="jungkook" />
+                </span>
+            </div>
+
+
+
+
+
+        </div>
+
+    );
+}
+export default Navbar;
---
+++ src/component/photogallery.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const photogallery = () => {
+  return (
+    <div>photogallery</div>
+  )
+}
